82f03c38636c8f4bc053e2307814503f67f345d4
Eirik S. Morland <eirik@morland.no>
2015-07-01 17:17:17 +0200

    Add known hops and test it.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 'use strict';
+
+const knownHopsAlpha = {
+  'centennial': 10
+};
+
 export default class {
-  constructor(name, alpha) {
+  constructor(name, alpha = null) {
     this.name = name;
+    if (!alpha) {
+      // Normalize name.
+      var index = name.toLowerCase();
+      alpha = knownHopsAlpha[index];
+    }
     this.alpha = alpha;
   }
   getAlpha() {
--- test/test.js
+++ test/test.js
@@ -5,4 +5,10 @@ describe('Module functionality', function() {
   it('Should export the expected type', function() {
     Hop.should.be.instanceOf(Function);
   });
+  it('Should construct and hold the expected value', () => {
+    new Hop('test', 12).getAlpha().should.equal(12);
+  });
+  it('Should construct with alpha for known hops', () => {
+    new Hop('Centennial').getAlpha().should.equal(10);
+  });
 });
